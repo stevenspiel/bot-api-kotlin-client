a17b90db1ac5148523ac18ede78f1103bf729a48
Steven Spiel <stevenspiel@gmail.com>
2025-01-10 12:37:29 -0500

    add members to deposit entry request

--- library/src/main/kotlin/one/mixin/bot/vo/DepositEntryRequest.kt
+++ library/src/main/kotlin/one/mixin/bot/vo/DepositEntryRequest.kt
@@ -5,4 +5,6 @@ import com.google.gson.annotations.SerializedName
 data class DepositEntryRequest(
     @SerializedName("chain_id")
     val chainId: String,
+    @SerializedName("members")
+    val members: List<String>?,
 )
